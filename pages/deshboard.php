384d90f8eb534d361fe075118ea06f07301c07ee
arjunpatel1 <33666170+arjunpatel1@users.noreply.github.com>
2019-04-22 06:01:27 -0400

    PHP
    Maxicon

---
+++ pages/deshboard.php
@@ -0,0 +1,258 @@
+
+<div id="home">
+    <!-- Start Slider -->
+    <div id="da-slider" class="da-slider">
+        <div class="mask"></div>
+        <div class="container">
+            <div class="da-slide effect_hover">
+                <h2 class="fittext2">Welcome to ITWebco</h2>
+                <h4>Clean & responsive</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a href="#" class="da-link button ">Read more</a>
+                <div class="da-img">
+                    <img src="slider/32714-6-apple-computer (1).png" alt="image01" width="320">
+                </div>
+            </div>
+            <!-- End first slide -->
+            <div class="da-slide">
+                <h2>Easy management</h2>
+                <h4>Easy to use</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a href="#" class="da-link button">Read more</a>
+                <div class="da-img">
+                    <img src="slider/mobile-phones (1).png" width="320" alt="image02">
+                </div>
+            </div>
+            <!-- End second slide -->
+            <!-- Start third slide -->
+            <div class="da-slide">
+                <h2>Revolution</h2>
+                <h4>Customizable</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a href="#" class="da-link button">Read more</a>
+                <div class="da-img">
+                    <img src="slider/mac-computer-screen-png-apple-computer-device-display-laptop-mac-macbook-macbook-pro-1024 (1).png" width="320" alt="image03">
+                </div>
+            </div>
+            <!-- Start third slide -->
+            <!-- Start cSlide navigation arrows -->
+            <div class="da-arrows">
+                <span class="da-arrows-prev"></span>
+                <span class="da-arrows-next"></span>
+            </div>
+            <!-- End cSlide navigation arrows -->
+        </div>
+    </div>
+</div>
+<!-- End home section -->
+<!-- Service section start -->
+<div class="section primary-section" id="service" style="background: #f4f4f4;padding: 1em;">
+    <div class="container" style="background: white">
+        <div class="svg-wrapper">
+            <svg height="60" width="280" xmlns="http://www.w3.org/2000/svg">
+                <rect class="shape" height="60" width="280" />
+                <div class="text">What We Do</div>
+            </svg>
+        </div>
+        <div class="row-fluid" style="margin-top: -3pc;margin-bottom: 20px">
+            <article class="span6">
+                <div class="about_img">
+
+                </div>
+            </article>
+            <article class="span6 res_about">
+                <h1 class="about_title"><span>About Us</span></h1>
+                <div class="content content_graph">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </article>
+        </div>
+    </div>
+</div>
+<!-- Service section end -->
+<!-- Portfolio section start -->
+<div class="section secondary-section" style="background: #f4f4f4;padding: 0;padding-top: 3pc" id="services">
+    <div class="container text-center" id="services_content">
+        <h1>Our Services <span></span></h1>
+        <article class="ser_res">
+            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 service_list">
+                <div class="our-services button-8 text-center">
+                    <a>
+                        <div class="eff-8"></div>
+                        <img src="img/ecommerce.png" alt="ecommerce">
+                        <h4>E-Commerce</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 service_list">
+                <div class="our-services text-center button-8">
+                    <a>
+                        <div class="eff-8"></div>
+                        <img src="img/javascript (1).png" alt="javascript development">
+                        <h4>Javascript</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 service_list">
+                <div class="our-services text-center button-8">
+                    <a>
+                        <div class="eff-8"></div>
+                        <img src="img/erp.png" alt="erp">
+                        <h4>ERP</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 service_list">
+                <div class="our-services text-center button-8">
+                    <a>
+                        <div class="eff-8"></div>
+                        <img src="img/cms.png" alt="cms">
+                        <h4>CMS</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 service_list">
+                <div class="our-services text-center button-8">
+                    <a>
+                        <div class="eff-8"></div>
+                        <img src="img/software.png" alt="software">
+                        <h4>Software</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 service_list ">
+                <div class="our-services text-center button-8">
+                    <div class="eff-8"></div>
+                    <a>
+                        <img src="img/web-app.png" alt="web-app">
+                        <h4>Mobile App</h4>
+                    </a>
+                </div>
+            </div>
+        </article>
+        <div class="new_service_div">
+            <h1>CONTENT REQUIRE ?</h1>
+            <a href="">
+                <b>
+                    <button class="button button--nanuk button--text-thick button--text-upper button--size-s button--border-thick">
+                        <span>C</span><span>L</span><span>I</span><span>C</span><span>K</span> <span> M</span><span>E</span>
+                    </button>
+                </b>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="section secondary-section ser_height" id="portfolio">
+</div>
+<div>
+</div>
+<div class="svg-rotate">
+    <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" class="wave"><title>Wave</title><defs></defs><path id="feel-the-wave" d=""/></svg>
+    <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" class="wave"><title>Wave</title><defs></defs><path id="feel-the-wave-two" d=""/></svg>
+</div>
+
+<?  include ('included/portfolio.html'); ?>
+<div class="section third-section">
+    <div class="container newsletter">
+        <div class="sub-section">
+            <div class="title clearfix">
+                <div class="pull-left">
+                    <h3>Newsletter</h3>
+                </div>
+            </div>
+        </div>
+        <div id="success-subscribe" class="alert alert-success invisible">
+            <strong>Well done!</strong>You successfully subscribet to our newsletter.</div>
+        <div class="row-fluid">
+            <div class="span5">
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+            </div>
+            <div class="span7">
+                <form class="inline-form">
+                    <input type="email" name="email" id="nlmail" class="span8" placeholder="Enter your email" required />
+                    <button id="subscribe" class="button button--nanuk button--text-thick button--text-upper button--size-s button--border-thick button--nanuk">
+                        <span>S</span>
+                        <span>u</span>
+                        <span>b</span>
+                        <span>s</span>
+                        <span>c</span>
+                        <span>r</span>
+                        <span>i</span>
+                        <span>b</span>
+                        <span>e</span>
+                    </button>
+                </form>
+                <div id="err-subscribe" class="error centered">Please provide valid email address.</div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Newsletter section end -->
+<!-- Contact section start -->
+<div id="contact" class="contact">
+    <div class="section secondary-section" style="background:#f4f4f4;padding-bottom: 0;padding-top: 1px;">
+        <div class="map-wrapper">
+            <div class="container" style="background: white;padding: 10px;margin-top: 22px;margin-bottom: 22px;">
+                <span class="contact2-form-title">Contact Us <i class="far fa-arrow-alt-circle-right"></i></span>
+                <div class="row-fluid">
+                    <div class="span7">
+                        <div class="span12 contact-img">
+                            <img src="slider/contact_us.png">
+                        </div>
+                    </div>
+                    <div class="span5 contact-form centered animated bounceIn back-contact">
+                        <h3 style="color: white;">Say Hello</h3>
+                        <div id="successSend" class="alert alert-success invisible">
+                            <strong>Well done!</strong>Your message has been sent.</div>
+                        <div id="errorSend" class="alert alert-error invisible">There was an error.</div>
+                        <form id="contact-form" action="php/mail.php">
+                            <div class="control-group">
+                                <div class="controls">
+                                    <input class="span12" type="text" id="name" name="name" placeholder="* Your name...">
+                                    <div class="error left-align" id="err-name">Please enter name.</div>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="controls">
+                                    <input class="span12" type="email" name="email" id="email" placeholder="* Your email...">
+                                    <div class="error left-align" id="err-email">Please enter valid email adress.</div>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="controls">
+                                    <textarea class="span12" name="comment" id="comment" placeholder="* Comments..."></textarea>
+                                    <div class="error left-align" id="err-comment">Please enter your comment.</div>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="controls effect_hover">
+                                    <button class="btn-11 message-btn" id="send-mail" style="transition: all 0.3s ease">Send message <span><i class="fas fa-angle-double-right"></i></span></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
